ac7c3427518e7f30867725fa6680ee7399f3d865
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-02-02 18:55:59 +0600

    double vs three equal

---
+++ DoubleVsThreeEqual.js
@@ -0,0 +1,21 @@
+
+let number=2;
+let num='2';
+
+if(number==num)
+{
+    console.log('Condition ture');
+}
+else
+{
+    console.log('Condition False');
+}
+// Thre Condition
+if(number===num)
+{
+    console.log('Condition ture');
+}
+else
+{
+    console.log('Condition False');
+}
