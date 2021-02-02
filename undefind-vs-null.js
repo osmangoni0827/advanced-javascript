40a52e3c7449d04c7f5099fe5a235330e54b62c6
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-02-02 14:07:16 +0600

    undefind vs null

---
+++ undefind-vs-null.js
@@ -0,0 +1,25 @@
+
+let number;
+
+function add(num1,num2)
+{
+    num1+num2;
+}
+
+function add(num1,num2)
+{
+   console.log(num2,num1);
+}
+const result=add(25);
+
+const picture={one:'pic1',two:'pic2',thre:'pic3'}
+console.log(picture.fur);
+
+const picture=[45,25,41,52,84]
+console.log(picture[20]);
+
+
+
+                // Null
+let x=null;
+console.log(x);
