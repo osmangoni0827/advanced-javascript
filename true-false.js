5c46ff6a769b9b6d787fad5f8f15a8b1f995f890
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-02-02 11:54:59 +0600

    added

---
+++ true-false.js
@@ -0,0 +1,24 @@
+
+            //False value
+//0
+//'' Empty String
+//undefined
+//NaN
+//null
+//false
+
+            //Truth Value
+//[]
+//' '
+//'0'
+//{}
+let num={};
+console.log(num);
+if(num)
+{
+    console.log('Condition is truth');
+}
+else
+{
+    console.log('Condition is false');
+}
